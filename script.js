5e5ca21fc92fb92323a967aba8b45ff8f4b8eb92
Love Gupta <90507983+Lovegupta112@users.noreply.github.com>
2023-06-10 12:39:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let arr=shortHex.split("");
+	let res='';
+	for(let i=1;i<arr.length;i++){
+		res=res+arr[i];
+		res=res+arr[i];
+	}
+	
+	return arr[0]+res;
 };
 
 // Do not change the code below.
