d3e7acf8a8a578d79ad3cc5d97359763cd5c0eff
Love Gupta <90507983+Lovegupta112@users.noreply.github.com>
2023-06-10 12:55:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const extendHex = (shortHex) => {
   // write your code here
 	shortHex=shortHex.toLowerCase();
+	
 	let arr=shortHex.split("");
 	let res='';
+	if(arr[0]==='#'){
 	for(let i=1;i<arr.length;i++){
 		res=res+arr[i]+arr[i];
 		// res=res+arr[i]; 
 	}
 	res=arr[0]+res;
+	}
+	else{
+		for(let i=0;i<arr.length;i++){
+		res=res+arr[i]+arr[i];
+		// res=res+arr[i]; 
+	}
+	}
 	return res.toLowerCase();
 };
   
